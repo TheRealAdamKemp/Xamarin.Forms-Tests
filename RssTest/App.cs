5998ee906c7f1504e10ad26380a7f2b4b6442afa
Adam Kemp <adam@adamkemp.com>
2014-07-22 21:36:49 -0500

    Initial commit.

---
+++ RssTest/App.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using Xamarin.Forms;
+
+using HigLabo.Net.Rss;
+
+namespace RssTest
+{
+    public class App
+    {
+        private static ListPage _listPage;
+
+        public static Page GetMainPage()
+        {
+            _listPage = new ListPage()
+                {
+                    ItemsSource = Enumerable.Repeat("Loading...", 1),
+                };
+            LoadItemsAsync(new Uri("http://feeds.macrumors.com/MacRumors-All?format=xml"));
+            return _listPage;
+        }
+
+        public static async void LoadItemsAsync(Uri feedUri)
+        {
+            var client = new RssClient();
+            var feed = await client.GetRssFeedAsync(feedUri);
+            _listPage.ItemsSource = feed.Items.Select(item => item.Title);
+        }
+    }
+}
+
---
+++ RssTest/ListPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace RssTest
+{    
+    public partial class ListPage : ContentPage
+    {    
+        public ListPage ()
+        {
+            InitializeComponent ();
+        }
+
+        public IEnumerable<object> ItemsSource
+        {
+            get
+            {
+                return _listView.ItemsSource.Cast<object>();
+            }
+
+            set
+            {
+                _listView.ItemsSource = value;
+            }
+        }
+    }
+}
+
